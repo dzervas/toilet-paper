459fbfc28e449bf487a3b4bef7afd7f7e4651258
Dimitris Zervas <dzervas@dzervas.gr>
2014-11-18 21:24:41 +0200

    Bug fixes in sudoku

--- hy100/hw3b.c
+++ hy100/hw3b.c
@@ -3,41 +3,51 @@
 unsigned char unique(int *array);
 
 int main(int argc, char *argv[]) {
-	int input[9][9], buffer[9];
+	int input[9][9], buffer[9], errors[3][9] = { { 0 }, { 0 }, { 0 } };
 	unsigned char i, j, k;
 
 	for (i = 0; i < 9; i++) {
 		if (scanf("%d %d %d %d %d %d %d %d %d", &input[i][0], &input[i][1], &input[i][2],
 					&input[i][3], &input[i][4], &input[i][5], &input[i][6],
-					&input[i][7], &input[i][8]) != 8)
+					&input[i][7], &input[i][8]) != 9)
 			return 1;
 	}
 
 	for (i = 0; i < 9; i++) {
 		/* Line check */
 		if (!unique(input[i]))
-			printf("Error in line: %d", i);
+			printf("Error in line: %d\n", i + 1);
+		else
+			printf("Line %d\n", i + 1);
 
 		/* Row check */
 		for (j = 0; j < 9; j++)
 			buffer[j] = input[i][j];
 
 		if (!unique(buffer))
-			printf("Error in row: %d", i);
+			printf("Error in row: %d\n", i + 1);
+		else
+			printf("Row %d\n", i + 1);
 
 		/* Block check */
 		if ((i % 3) == 0) {
 			for (j = 0; j < 9; j++) {
-				for (k = 0; k < 3; k++)
-					buffer[j] = input[i*3 + k][j];
+				for (k = 0; k < 3; k++) {
+					buffer[(j % 3) * 3 + k] = input[(i % 3) *3 + k][j];
+					printf("%d = %d[%d]\n", (j % 3) * 3 + k, (i % 3) * 3 + k, j);
+				}
 
 				if ((j % 3) == 2) {
 					if (!unique(buffer))
-						printf("Error in block: %d", 1 + i + j / 3);
+						printf("Error in block: %d\n", 1 + i + j / 3);
+					else
+						printf("Block %d\n", 1 + i + j / 3);
 				}
 			}
 		}
 	}
+
+	return 0;
 }
 
 unsigned char unique(int *array) {
@@ -47,10 +57,13 @@ unsigned char unique(int *array) {
 		buff[i] = 0;
 
 	for (i = 0; i < 9; i++) {
-		if (buff[array[i]] != 0)
+		/*printf("%d\n", array[i] -1);*/
+		if (array[i] == 0)
+			printf("!!!");
+		if (buff[array[i] - 1] != 0)
 			return 0;
 
-		buff[array[i]]++;
+		buff[array[i] - 1]++;
 	}
 
 	return 1;
