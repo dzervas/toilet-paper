db3adb61b4ed312bd78531d7ccae8d867b41dad4
Dimitris Zervas <dzervas@dzervas.gr>
2014-11-18 14:29:25 +0200

    Kinda usable code (not working yet)

--- hy100/hw3b.c
+++ hy100/hw3b.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+	unsigned char i, buffer[];
 
+	for (i = 0; i < 9; i++) {
+		if (sscanf("%d %d %d %d %d %d %d %d %d", buffer[i], buffer[i]+1, buffer[i]+2,
+					buffer[i]+3, buffer[i]+4, buffer[i]+5, buffer[i]+6,
+					buffer[i]+7, buffer[i]+8) != 8)
+			return 1;
+	}
+
+	for (i = 0; i < 9; i++) {
+		// Line check
+		if (!unique(buffer[i]))
+			printf("Error in line: %d", i);
+		// Row check
+		// Block check
+	}
 }
 
 unsigned char unique(unsigned char *array) {
-	unsigned char buffer[9], i;
+	unsigned char buff[9], i;
+
+	for (i = 0; i < 9; i++)
+		buff[i] = 0;
 
 	for (i = 0; i < 9; i++) {
-		if (buffer[array[i]] != 0)
-			printf("Error here!");
+		if (buff[array[i]] != 0)
+			return 0;
 
-		buffer[array[i]]++;
+		buff[array[i]]++;
 	}
+
+	return 1;
 }
