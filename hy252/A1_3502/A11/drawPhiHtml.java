d6874d0d04dc473a2cbb6306dde112cea6c1041d
Dimitris Zervas <dzervas@dzervas.gr>
2016-09-28 16:23:21 +0300

    [252][A11] 4th problem solved

--- hy252/A1_3502/A11/drawPhiHtml.java
+++ hy252/A1_3502/A11/drawPhiHtml.java
@@ -3,9 +3,9 @@ import java.io.*;
 public class drawPhiHtml {
     public static void main(String[] args) throws IOException {
 	Integer tmp = Integer.parseInt(args[0]) * 10;
-	PrintWriter out = new PrintWriter("PhiK.html", "UTF-8");
+	PrintWriter out = new PrintWriter("PhiF.html", "UTF-8");
 	out.print("<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />\n</head>\n<body>\n<p style=\"font-size:"
-		  + Integer.toString(tmp) + "\" align=\"center\">Φ</p>\n</body>\n</html>");
+		  + Integer.toString(tmp*10) + "%\" align=\"center\">F</p>\n</body>\n</html>");
 	out.close();
     }
 }
