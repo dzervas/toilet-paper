64033d8bb19c2825921221525dd4179121651925
Dimitris Zervas <dzervas@dzervas.gr>
2016-09-28 17:04:12 +0300

    [252][A12] 2nd problem solved

--- hy252/A1_3502/A12/PingPong.java
+++ hy252/A1_3502/A12/PingPong.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+import org.jfugue.Player;
 
 public class PingPong {
 	public static void main(String[] args) {
@@ -6,8 +7,18 @@ public class PingPong {
 	}
 }
 
+class Utilities {
+	public static void sound(int i) {
+		i = i*12;
+		Player p = new Player();
+		p.play("[" + i + "] [" + (i+2) + "] [" + (i+5) + "]");
+		return;
+	}
+}
+
 class Ping {
 	public static void ping(int i) {
+		Utilities.sound(i);
 		JOptionPane.showMessageDialog(null, Integer.toString(i), "Ping", JOptionPane.INFORMATION_MESSAGE);
 		if (i > 1)
 			Pong.pong(i - 1);
@@ -16,6 +27,7 @@ class Ping {
 
 class Pong {
 	public static void pong(int i) {
+		Utilities.sound(i);
 		JOptionPane.showMessageDialog(null, Integer.toString(i), "Pong", JOptionPane.INFORMATION_MESSAGE);
 		if (i > 1)
 			Ping.ping(i - 1);
