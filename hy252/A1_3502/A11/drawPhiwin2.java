be2ec074a6a330efdb5b2d7d05cb91821068a639
Dimitris Zervas <dzervas@dzervas.gr>
2016-09-28 16:18:03 +0300

    [252][A11] 3rd problem solved

--- hy252/A1_3502/A11/drawPhiwin2.java
+++ hy252/A1_3502/A11/drawPhiwin2.java
@@ -2,46 +2,45 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class drawPhiwin2 {
-    private static Integer lines; // Number of lines to draw (user input)
-    private static Integer limit; // Which line(s) will be the largest
-    // Line in case of odd and lines in case of even number of lines
-    // Limit also serves as the vertical separator of the bitmap
+	private static Integer lines; // Number of lines to draw (user input)
+	private static Integer limit; // Which line & column will be the middle
 
-    // HACK: Took the function from the previous hw and multiplied spaces left & right
-    //           by 2.
-    private static StringBuilder algorithm(int line, char character) {
-	StringBuilder out = new StringBuilder("");
-	int i;
+	private static StringBuilder algorithm(int line, char character) {
+		StringBuilder out = new StringBuilder("");
+		int i;
 
-	// Allocate the string columns with spaces
-	for (i = 0; i < (limit * 4 + 1); i++)
-	    out.append(' ');
-	out.append('\n');
+		if (line == 0) {
+			for (i = 0; i < lines; i++)
+				out.append(character);
+		} else if (line == limit - 1) {
+			for (i = 0; i < limit; i++)
+				out.append(character);
+		} else {
+			out.append(character);
+		}
 
-	if (line > limit)
-	    line = lines - line - 1;
+		return out;
+	}
 
-	out.setCharAt(2 * (limit - line), character); // Paint the left bit
-	out.setCharAt(2 * limit, character); // Paint the middle bit
-	out.setCharAt(2 * (limit + line), character); // Paint the right bit
+	public static void main(String[] args) {
+		do {
+			StringBuilder tmp = new StringBuilder("");
+			lines = Integer.parseInt(JOptionPane.showInputDialog("Give me a number ", 4));
 
-	return out;
-    }
+			if (lines < 4 || lines > 9) {
+				JOptionPane.showMessageDialog(null, "Allowed range is 4-9", "DrawFwin", JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
 
-    public static void main(String[] args) {
-	do {
-	    StringBuilder tmp = new StringBuilder("");
-	    lines = Integer.parseInt(JOptionPane.showInputDialog("Give me a number ", 4));
+			limit = (lines / 2) + 1;
+			for (int i = 0; i < lines; i++) {
+				// Add 49 to convert character to ASCII
+				// 49 is 0 in ASCII, so 49 + i == ascii_char(i + 1)
+				tmp.append(algorithm(i, (char) (i+49)));
+				tmp.append("\n");
+			}
 
-	    if (lines < 4 || lines > 9)
-		System.exit(0);
-
-	    limit = (lines / 2) + (lines % 2) - 1;
-	    for (int i = 0; i < lines; i++)
-		// HACK: Turn int into char through ASCII table
-		tmp.append(algorithm(i, (char) (i + 1 + 48)));
-
-	    JOptionPane.showMessageDialog(null, tmp.toString(), "Output window", JOptionPane.INFORMATION_MESSAGE);
-	} while (lines >= 4 && lines <= 30);
-    }
+			JOptionPane.showMessageDialog(null, tmp.toString(), "Output window", JOptionPane.INFORMATION_MESSAGE);
+		} while (true);
+	}
 }
