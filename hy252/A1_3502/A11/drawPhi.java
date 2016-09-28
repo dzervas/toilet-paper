f46351b28f74f8e8b9a6f2ba9c259fc42bfa0086
Dimitris Zervas <dzervas@dzervas.gr>
2016-09-28 15:52:19 +0300

    [252][A11] Done

---
+++ hy252/A1_3502/A11/drawPhi.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class drawPhi {
+	private static Integer lines; // Number of lines to draw (user input)
+	private static Integer limit; // Which line & column will be the middle
+
+	// Reusable for the next homework
+	private static StringBuilder algorithm(int line, char character) {
+		StringBuilder out = new StringBuilder("");
+		int i;
+
+		if (line == 0) {
+			for (i = 0; i < lines; i++)
+				out.append(character);
+		} else if (line == limit - 1) {
+			for (i = 0; i < limit; i++)
+				out.append(character);
+		} else {
+			out.append(character);
+		}
+
+		return out;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		lines = Integer.parseInt(args[0]);
+
+		do {
+			if (lines < 4 || lines > 30)
+				break;
+
+			limit = (lines / 2) + 1;
+			for (int i = 0; i < lines; i++)
+				System.out.println(algorithm(i, '*'));
+
+			System.out.println("Feed me moar: ");
+			lines = in.nextInt();
+		} while (lines >= 4 && lines <= 30);
+	}
+}
