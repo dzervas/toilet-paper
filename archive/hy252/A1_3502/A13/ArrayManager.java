c6efa147eba5872d0a0cd6da03028ee3a15317c8
Dimitris Zervas <dzervas@dzervas.gr>
2016-10-05 14:27:59 +0300

    [252][A13] Fix concat

--- archive/hy252/A1_3502/A13/ArrayManager.java
+++ archive/hy252/A1_3502/A13/ArrayManager.java
@@ -2,37 +2,37 @@ import java.util.Random;
 import org.jfugue.Player;
 
 public class ArrayManager {
-    private static Random rand;
+	private static Random rand;
 
-    public static void fillRandom(int min, int max, int[] ar) {
-	// NOTE: nextInt is exclusive, that's why the +1
-	for (int i = 0; i < ar.length; i++)
-	    ar[i] = rand.nextInt(max - min + 1) + min;
-    }
-
-    public static void fillFrom(int[] ar, int[] fromArr) {
-	if (ar.length != fromArr.length) {
-	    System.out.println("Oops... Arrays are not of the same size!");
-	    return;
+	public static void fillRandom(int min, int max, int[] ar) {
+		// NOTE: nextInt is exclusive, that's why the +1
+		for (int i = 0; i < ar.length; i++)
+			ar[i] = rand.nextInt(max - min + 1) + min;
 	}
 
-	for (int i = 0; i < ar.length; i++)
-	    ar[i] = fromArr[i];
-    }
+	public static void fillFrom(int[] ar, int[] fromArr) {
+		if (ar.length != fromArr.length) {
+			System.out.println("Oops... Arrays are not of the same size!");
+			return;
+		}
 
-    public static void playMusic(int[] ar) {
-	Player p = new Player();
+		for (int i = 0; i < ar.length; i++)
+			ar[i] = fromArr[i];
+	}
 
-	for (int i = 0; i < ar.length; i++)
-	    p.play("[" + ar[i] + "]");
-    }
+	public static void playMusic(int[] ar) {
+		Player p = new Player();
 
-    public static void main(String[] args) {
-	int[] p1 = new int[20], p2 = new int[20];
-	rand = new Random();
+		for (int i = 0; i < ar.length; i++)
+			p.play("[" + ar[i] + "]");
+	}
 
-	fillRandom(50, 120, p1);
-	fillFrom(p2, p1);
-	playMusic(p2);
-    }
+	public static void main(String[] args) {
+		int[] p1 = new int[20], p2 = new int[20];
+		rand = new Random();
+
+		fillRandom(50, 120, p1);
+		fillFrom(p2, p1);
+		playMusic(p2);
+	}
 }
---
+++ hy252/A1_3502/A13/MusicMaker.java
@@ -0,0 +1,73 @@
+import org.jfugue.Player;
+
+public class MusicMaker {
+	static int[] concat(int[] a, int[] b) {
+		int result[];
+
+		result = new int[a.length + b.length];
+
+		//System.out.print("\na: ");
+		for (int i = 0; i < a.length; i++) {
+			//System.out.print(a[i]);
+			result[i] = a[i];
+		}
+
+		//System.out.print("\nb: ");
+		for (int i = 0; i < b.length; i++) {
+			//System.out.print(b[i]);
+			result[a.length + i] = b[i];
+		}
+
+		return result;
+	}
+
+	// I assume that "by" will ALWAYS be of 3 elemts long
+	static int[] expand(int[] input, int[] by) {
+		int result[];
+
+		result = new int[input.length * 3];
+
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < by.length; j++) {
+				result[(i * by.length) + j] = input[i] + by[j];
+			}
+		}
+		System.out.println();
+
+		return result;
+	}
+
+	static int[] expandHappy(int[] input) {
+		return expand(input, new int[]{0, 4, 7});
+	}
+
+	static int[] expandSad(int[] input) {
+		return expand(input, new int[]{0, 3, 7});
+	}
+
+	static void playArray(int a[]) {
+		StringBuilder result = new StringBuilder("");
+
+		for (int i = 0; i < a.length; i++) {
+			result.append("[" + a[i] + "] ");
+		}
+
+		System.out.println(result);
+		//Player p = new Player();
+		//p.play(result.toString());
+	}
+
+	public static void main(String[] args) {
+		int seed1[] = {80, 80};
+		int seed2[] = {80, 92, 104};
+		int seed3[] = {70,70,74,77,77};
+
+		playArray(seed1);
+		playArray(expandHappy(seed1));
+		playArray(expandSad(seed1));
+		playArray(expandHappy(seed2));
+		playArray(concat(seed1,
+					concat(expandHappy(seed3),expandSad(seed2))));
+
+	}
+}
