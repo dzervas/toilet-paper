1d32df03029fdd62b13fd9c0045ac83cd9162c83
Dimitris Zervas <dzervas@dzervas.gr>
2017-04-22 15:32:32 +0300

    [457][ass2] Add hash and file access

--- hy457/assign_2/Task_A.c
+++ hy457/assign_2/Task_A.c
@@ -4,11 +4,21 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <openssl/md5.h>
+#include <sys/stat.h>
 
 void log_action(const char *path, unsigned const char is_access) {
+	FILE *(*_fopen)(const char*, const char*);
 	char ttime[80], tdate[80];
+	struct stat statinfo;
 	struct tm *timeinfo;
 	time_t rawtime;
+	unsigned char hash[MD5_DIGEST_LENGTH];
+
+	FILE *fp;
+	char buffer[1024];
+	int md5count;
+	MD5_CTX mdContext;
 
 	/* Get time */
 	time(&rawtime);
@@ -17,15 +27,26 @@ void log_action(const char *path, unsigned const char is_access) {
 	strftime(ttime, 80, "%T", timeinfo);
 	strftime(tdate, 80, "%F", timeinfo);
 
+	/* Print the actual log */
 	printf("UID\tfile_name\tdate\t\ttime\t\topen\taction_denied\thash\n");
-	printf("%d\t%s\t%s\t%s\t%d\t%d\t\t%s\n",
-			(unsigned int) getuid(),
-			path,
-			tdate,
-			ttime,
-			is_access,
-			0,
-			"hash");
+	printf("%d\t%s\t%s\t%s\t%d\t%d\t\t", (unsigned int) getuid(), path, tdate,
+			ttime, is_access, -1 * stat(path, &statinfo));
+
+	/* Calculate and print hash */
+	if (is_access) {
+		_fopen = dlsym(RTLD_NEXT, "fopen");
+		fp = _fopen(path, "rb");
+
+		MD5_Init(&mdContext);
+		while ((md5count = fread(buffer, 1, 1024, fp)))
+			MD5_Update(&mdContext, buffer, md5count);
+		MD5_Final(hash, &mdContext);
+
+		for(md5count = 0; md5count < MD5_DIGEST_LENGTH; md5count++)
+			printf("%02x", hash[md5count]);
+	}
+
+	printf("\n");
 }
 
 void plog_action(int ptr, unsigned const char is_access) {
