b8d268b923d75744da787fe4424fa6f1a442c940
Dimitris Zervas <dzervas@dzervas.gr>
2017-04-23 21:10:08 +0300

    [457][ass2] Add log file writing

--- hy457/assign_2/Task_A.c
+++ hy457/assign_2/Task_A.c
@@ -11,7 +11,8 @@
 
 void log_action(const char *path, unsigned const char is_access) {
 	FILE *(*_fopen)(const char*, const char*);
-	char ttime[80], tdate[80], abspath[1024];
+	FILE *lp;
+	char ttime[80], tdate[80], abspath[1024], logbuff[1024];
 	struct stat statinfo;
 	struct tm *timeinfo;
 	time_t rawtime;
@@ -21,6 +22,8 @@ void log_action(const char *path, unsigned const char is_access) {
 	unsigned char hash[MD5_DIGEST_LENGTH];
 	int md5count;
 	MD5_CTX mdContext;
+
+	_fopen = dlsym(RTLD_NEXT, "fopen");
 
 	/* Get absolute path */
 	realpath(path, abspath);
@@ -33,29 +36,41 @@ void log_action(const char *path, unsigned const char is_access) {
 	strftime(tdate, 80, "%F", timeinfo);
 
 	/* Print the actual log */
-	printf("UID\tfile_name");
+	sprintf(logbuff, "UID\tfile_name");
 
-	for (unsigned long i = 0; i < (strlen(abspath)/8); i++) printf("\t");
+	for (unsigned long i = 0; i < (strlen(abspath)/8); i++)
+		sprintf(logbuff + strlen(logbuff), "\t");
 
-	printf("date\t\ttime\t\topen\taction_denied\thash\n");
-	printf("%d\t%s\t%s\t%s\t%d\t%d\t\t", (unsigned int) getuid(), abspath, tdate,
+	sprintf(logbuff + strlen(logbuff), "date\t\ttime\t\topen\taction_denied\thash\n");
+	sprintf(logbuff + strlen(logbuff), "%d\t%s\t%s\t%s\t%d\t%d\t\t", (unsigned int) getuid(), abspath, tdate,
 			ttime, is_access, -1 * stat(abspath, &statinfo));
 
 	/* Calculate and print hash */
-	if (is_access) {
-		_fopen = dlsym(RTLD_NEXT, "fopen");
-		fp = _fopen(abspath, "rb");
+	fp = _fopen(abspath, "rb");
 
+	if (fp) {
 		MD5_Init(&mdContext);
 		while ((md5count = fread(buffer, 1, 1024, fp)))
 			MD5_Update(&mdContext, buffer, md5count);
 		MD5_Final(hash, &mdContext);
 
 		for(md5count = 0; md5count < MD5_DIGEST_LENGTH; md5count++)
-			printf("%02x", hash[md5count]);
-	}
+			sprintf(logbuff + strlen(logbuff), "%02x", hash[md5count]);
+	} else
+		sprintf(logbuff + strlen(logbuff), "(none)");
 
-	printf("\n");
+	fclose(fp);
+
+	sprintf(logbuff + strlen(logbuff), "\n");
+
+	/* Open log for writing */
+	lp = _fopen("log.txt", "a");
+	if (!lp) {
+		sprintf(logbuff + strlen(logbuff), "Could not open log.txt\n");
+		return;
+	}
+	fputs(logbuff, lp);
+	fclose(lp);
 }
 
 void plog_action(int ptr, unsigned const char is_access) {
@@ -69,12 +84,12 @@ void plog_action(int ptr, unsigned const char is_access) {
 	log_action(path, is_access);
 }
 
-void flog_action(const void *ptr, unsigned const char is_access) {
-	plog_action(fileno((FILE *) ptr), is_access);
+void flog_action(FILE *ptr, unsigned const char is_access) {
+	plog_action(fileno(ptr), is_access);
 }
 
 int open(const char *path, int oflag, ...) {
-	int (*_func)(const char*, int, ...);
+	int (*_func)(const char *, int, ...);
 
 	log_action(path, 1);
 
@@ -83,7 +98,7 @@ int open(const char *path, int oflag, ...) {
 }
 
 FILE *fopen(const char *path, const char *mode) {
-	FILE *(*_func)(const char*, const char*);
+	FILE *(*_func)(const char *, const char *);
 
 	log_action(path, 1);
 
@@ -92,7 +107,7 @@ FILE *fopen(const char *path, const char *mode) {
 }
 
 ssize_t write(int fildes, const void *buf, size_t nbyte) {
-	ssize_t (*_func)(int, const void*, size_t);
+	ssize_t (*_func)(int, const void *, size_t);
 
 	plog_action(fildes, 0);
 
@@ -101,10 +116,14 @@ ssize_t write(int fildes, const void *buf, size_t nbyte) {
 }
 
 size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) {
-	size_t (*_func)(const void*, size_t, size_t, FILE*);
+	size_t (*_func)(const void *, size_t, size_t, FILE *);
+	size_t result;
+
+	_func = dlsym(RTLD_NEXT, "fwrite");
+	result = (*_func)(ptr, size, nmemb, stream);
+	fflush(stream);
 
 	flog_action(stream, 0);
 
-	_func = dlsym(RTLD_NEXT, "fwrite");
-	return (*_func)(ptr, size, nmemb, stream);
+	return result;
 }
