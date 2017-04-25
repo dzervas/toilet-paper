c87ba6e2e5bf97141db95a2e27c79421304a1eee
Dimitris Zervas <dzervas@dzervas.gr>
2017-04-25 14:25:53 +0300

    [457][ass2] Got regex to work. 1st part done

--- hy457/assign_2/Task_B.c
+++ hy457/assign_2/Task_B.c
@@ -1,7 +1,17 @@
+#include <regex.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LOG_BUFFER 102400
+
+regmatch_t *regex_match(char *, char *);
 
 int main(int argc, char *argv[]) {
 	FILE *lp;
+	char log[MAX_LOG_BUFFER];
+
+	/* Self name is not needed */
+	argc--; argv++;
 
 	if (argc < 2) {
 		printf("./Task_B <log_path> <mode> [file_path]\n");
@@ -13,9 +23,12 @@ int main(int argc, char *argv[]) {
 		printf("Could not open log file\n");
 		return 1;
 	}
+	fread(log, 1, MAX_LOG_BUFFER, lp);
+	fclose(lp);
 
 	switch (argv[1][0]) {
 		case '1':
+			regex_match("([0-9]+)\t+.*\t+[0-9]\t+1", log);
 			break;
 		case '2':
 			break;
@@ -27,4 +40,24 @@ int main(int argc, char *argv[]) {
 	}
 
 	return 0;
+}
+
+regmatch_t *regex_match(char *needle, char *haystack) {
+	size_t ngroups;
+	regex_t compiled;
+	regmatch_t *groups;
+
+	regcomp(&compiled, needle, REG_EXTENDED);
+
+	ngroups = compiled.re_nsub + 1;
+	groups = malloc(ngroups * sizeof(regmatch_t));
+
+	if (regexec(&compiled, haystack, ngroups, groups, 0))
+		return NULL;
+
+	for (size_t i = 0; i < ngroups; i++) {
+		printf("%d - %d: %.4s\n", groups[i].rm_so, groups[i].rm_eo, haystack + groups[i].rm_so);
+	}
+
+	return groups;
 }
